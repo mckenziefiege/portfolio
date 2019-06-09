6ad2ab9d9e26ef7d3465488d6da538bc567a7ddf
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-06-09 17:27:40 -0400

    styled homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,17 @@ class Footer extends Component {
   render () {
     return (
       <div className="footer">
-        contact me
+      <div>
+        <p>Feel free to get in touch!</p>
+      </div>
+      <div>
+        <a className="footer__link" href="">mckenziefiege@gmail.com</a>
+      </div>
+      <div>
+        <a className="footer__link" href="">GitHub</a>
+        <a className="footer__link" href="">LinkedIn</a>
+        <a className="footer__link" href="">Blog</a>
+      </div>
       </div>
     );
   }
